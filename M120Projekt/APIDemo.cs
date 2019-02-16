505c4f6b7268d979b91494c04a859259cf47ea6c
unknown <michele.habegger@gawnet.ch>
2019-02-16 10:13:11 +0100

    try to push

---
+++ M120Projekt/APIDemo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M120Projekt
+{
+    static class APIDemo
+    {
+        #region Freund
+        // Create
+        public static void FreundCreate()
+        {
+            Debug.Print("--- FreundCreate ---");
+            // KlasseA
+            Data.Freund freund1 = new Data.Freund();
+            freund1.Vorname = "Lisa";
+            freund1.Nachname = "Friedli";
+            freund1.Adresse = "Blummenweg 5";
+            freund1.PLZ = 3366;
+            freund1.Ort = "Bettenhausen";
+            freund1.Geburtsdatum = new DateTime(1998,07,12);
+            freund1.Handynummer = "079 878 99 88";
+            freund1.Email = "lisa.friedlic@gamil.com";
+            freund1.Beziehungsstatus = false;
+            freund1.Beziehung = "Freund / Freundin";
+            freund1.BefreundetSeit = new DateTime(2011,09,15);
+            Int64 freund1Id = freund1.Erstellen();
+            Debug.Print("Freund erstellt mit Id:" + freund1Id);
+        }
+        public static void FreundCreateKurz()
+        {
+            Data.Freund freund2 = new Data.Freund { Vorname = "Tim", Nachname = "Sutter", Adresse = "Poststrasse 11",
+                   PLZ = 3000, Ort = "Bern", Geburtsdatum = DateTime.Today,  Beziehungsstatus = true};
+            Int64 freund2Id = freund2.Erstellen();
+            Debug.Print("Freund erstellt mit Id:" + freund2Id);
+        }
+
+        // Read
+        public static void FruendRead()
+        {
+            Debug.Print("--- FreundRead ---");
+            // Demo liest alle
+            foreach (Data.Freund freund in Data.Freund.LesenAlle())
+            {
+                Debug.Print("Freund Id:" + freund.FreundID + " Name:" + freund.Vorname);
+            }
+        }
+        // Update
+        public static void FreundUpdate()
+        {
+            Debug.Print("--- FreundUpdate ---");
+            // KlasseA ändert Attribute
+            Data.Freund freund = Data.Freund.LesenID(1);
+            freund.Ort = "Freund 1 nach Update";
+            freund.Aktualisieren();
+        }
+        // Delete
+        public static void FreundDelete()
+        {
+            Debug.Print("--- FreundDelete ---");
+            Data.Freund.LesenID(1).Loeschen();
+            Debug.Print("Freund mit Id 1 gelöscht");
+        }
+        #endregion
+    }
+}
---
+++ M120Projekt/Data/Freund.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace M120Projekt.Data
+{
+    public class Freund
+    {
+        #region Datenbankschicht
+        [Key]
+        public Int64 FreundID { get; set; }
+        [Required]
+        public String Vorname { get; set; }
+        [Required]
+        public String Nachname { get; set; }
+        [Required]
+        public String Adresse { get; set; }
+        [Required]
+        public Int64 PLZ { get; set; }
+        [Required]
+        public String Ort { get; set; }
+        [Required]
+        public DateTime Geburtsdatum { get; set; }
+        public String Handynummer { get; set; }
+        public String Email { get; set; }
+        public Boolean Beziehungsstatus { get; set; }
+        public String Beziehung { get; set; }
+        public DateTime BefreundetSeit { get; set; }
+
+        #endregion
+        #region Applikationsschicht
+        public Freund() { }
+        [NotMapped]
+        public String BerechnetesAttribut
+        {
+            get
+            {
+                return "Im Getter kann Code eingefügt werden für berechnete Attribute";
+            }
+        }
+        public static IEnumerable<Data.Freund> LesenAlle()
+        {
+            return (from record in Data.Global.context.Freund select record);
+        }
+        public static Data.Freund LesenID(Int64 klasseAId)
+        {
+            return (from record in Data.Global.context.Freund where record.FreundID == klasseAId select record).FirstOrDefault();
+        }
+        public static IEnumerable<Data.Freund> LesenAttributGleich(String suchbegriff)
+        {
+            return (from record in Data.Global.context.Freund where record.Ort == suchbegriff select record);
+        }
+        public static IEnumerable<Data.Freund> LesenAttributWie(String suchbegriff)
+        {
+            return (from record in Data.Global.context.Freund where record.Ort.Contains(suchbegriff) select record);
+        }
+        public Int64 Erstellen()
+        {
+            if (this.Ort == null || this.Ort == "") this.Ort = "leer";
+            if (this.Geburtsdatum == null) this.Geburtsdatum = DateTime.MinValue;
+            Data.Global.context.Freund.Add(this);
+            Data.Global.context.SaveChanges();
+            return this.FreundID;
+        }
+        public Int64 Aktualisieren()
+        {
+            Data.Global.context.Entry(this).State = System.Data.Entity.EntityState.Modified;
+            Data.Global.context.SaveChanges();
+            return this.FreundID;
+        }
+        public void Loeschen()
+        {
+            Data.Global.context.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+            Data.Global.context.SaveChanges();
+        }
+        public override string ToString()
+        {
+            return FreundID.ToString(); // Für bessere Coded UI Test Erkennung
+        }
+        #endregion
+    }
+}
